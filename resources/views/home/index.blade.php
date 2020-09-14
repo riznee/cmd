fb69ea9f08a0e7f8c7fbdb9e348cb448f99938bc
Mrinzee <m.rizny@gmail.com>
2020-09-14 09:58:12 +0500

    development

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,7 +11,7 @@
 					<h1 class="{{$article->category->description}}">{{$article->title}}</h1>
 					<span class="tag {{$article->category->color}}"> Type:  &nbsp; {{$article->category->title}} Updated:  &nbsp; {{$article->category->created_at}}</span>
 					<nav class="breadcrumb" aria-label="breadcrumbs">
-						<li><a href="#">Bulma</a></li>
+						<li><a href="{{route('page', $article->page->slug )}}">{{$article->page->title}}</a></li>
 					</nav>
 					{!!$article->content!!}
 				@endif
