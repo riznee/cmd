277feca8fa968cf2223b5ccd81df4a1411620742
Mrinzee <m.rizny@gmail.com>
2020-10-14 20:18:47 +0500

    chnage to vue start

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,41 @@ use Illuminate\Http\Request;
 // });
 
 
+// Application public Routs
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/page/{slug}', 'HomeController@page')->name('page');
+Route::post('/contactus/send', 'HomeController@contactSend')->name('contactus.send');
+
+// User Managment
+Route::get('/user/verify/{token}','UserController@userVerification');
+Route::get('user/password-reset/{token}', 'UserController@resetRequestVerified');
+Route::get('password-reset', 'UserController@resetPasswordView')->name('reset');
+Route::post('password-reset','UserController@sendResetRequest')->name('reset.post');
+Route::post('password-confirmation','UserController@resetPassword')->name('resetpassword.post');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('signup', 'UserController@register')->name('signup');
+Route::post('signup', 'UserController@registerRequest')->name('signup.post');
+
+
+
+
+// user Dashboards
+Route::group(['middleware' => ['auth']], function () {
+    
+    Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
+    Route::get('dashboard', 'AdminController@index')->name('dashboard');
+    Route::get('articles/{id}/publsih', 'ArticleController@publish')->name('articles.publish');
+    Route::get('articles/{id}/unpublsih', 'ArticleController@unPublish')->name('articles.unpublish');
+    Route::resource('articles', 'ArticleController');
+    Route::resource('settings', 'SettingController');
+    Route::resource('pages', 'PageController');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('users', 'UserController');
+    Route::resource('roles', 'RoleController');
+    Route::resource('contacts', 'ContactController');
+  
+});
+
+
