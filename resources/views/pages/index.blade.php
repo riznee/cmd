b4d26947534c531130bb591e01021dabb0ad7b62
Mrinzee <m.rizny@gmail.com>
2020-11-14 22:07:43 +0500

    development

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,106 +1,98 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container is-fluid">
-	<div class="wrapper">
-		<head class="p-6">
-			@include('partials.admiNav')
-		</head>
-		<section class="columns">
-			<div class="column is-one-fifth">
-				@include('partials.adminPanel')
-			</div>
-			<div class="column">
-				<div class="hero is-fullheight">
-					<div class="card">
-						<header class="card-header">
-						  <p class="card-header-title">
-							Pages
-						  </p>
-						  <a href="{{route('pages.create')}}" class="card-header-icon" aria-label="more options">
-							<span class="icon">
-							  <i class="fas fa-plus" aria-hidden="true"></i>
-							</span>
-						  </a>
-						</header>
-						<div class="card-content">
-							<table class="table  is-striped is-narrow is-hoverable is-fullwidth" style="justify-content: center">
-								<thead>
-									<tr>
-										<th>Slug</th>
-										<th>Parent</th>
-										<th> Depth</th>
-										<th> Title</th>
-										<th> Visible to Public </th>
-										<th>Created At</th>
-										<th>Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-									@if(!empty($pages))
-										@foreach($pages as $row)			
-											<tr>
-												<td>{{$row->slug}}</td>
-												<td>{{($row->parent->title ?? ' ')}}</td>
-												<td>{{$row->depth}}</td>
-												<td>{{$row->title}}</td>												
-												@if($row->visible == true )
-													<td>Yes</td>
-												@else
-													<td>No</td>
-												@endif
-												<td>{{$row->created_at}}</td>
-												<td>
-													<div class='columns'>
-														@can('pages-edit')
-														<div class='column'>
-															<a class=" button is-rounded is-link is-small" href="{{route('pages.show',$row['id'])}}">
-																<i class="fas fa-eye" aria-hidden="true"></i>
-																	&nbsp; View
-															</a> 
 
-														</div>
-														@endcan
+	<h2>Page</h2>
+    <div class="table-responsive">
+    	<table class="table table-striped table-sm table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>slug</th>
+              <th>Parent</th>
+              <th>Depth</th>
+              <th>Title</th>
+              <th>Visible to public</th>
+              <th>Created At </th>
+              <th>Updated At</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+			@if(!empty($pages))
+				@foreach($pages as $row)			
+					<tr>
+						<td>{{$row->id}}</td>
+						<td>{{$row->slug}}</td>
+						<td>{{($row->parent->title ?? ' ')}}</td>
+						<td>{{$row->depth}}</td>
+						<td>{{$row->title}}</td>
+						<td>
+							@if($row->visible == true)
+							<i class="fas fa-eye" aria-hidden="true"></i>
+							@can('pages-disable')
+							Change 
+								<a class="btn btn-outline-primary" href="{{route('pages.disable',$row['id'])}}">
+									<i class="far fa-eye-slash" aria-hidden="true"></i>
+									
+								</a>
+								@endcan 
+							@else
+							<i class="far fa-eye-slash" aria-hidden="true"></i>
+							@can('pages-enable')
+								Change		
+									<a class="btn btn-outline-primary" href="{{route('pages.enable',$row['id'])}}">
+										<i class="fas fa-eye" aria-hidden="true"></i>
+									</a>
+								@endcan
+							@endif  
+						</td>						
+						
+						<td>{{$row->created_at}}</td>
+						<td>{{$row->updated_at}}</td>
+						<td>
+							
+							@can('pages-edit')
+								<a class=" btn btn-outline-primary" href="{{route('pages.show',$row['id'])}}">
+									<i class="fas fa-pen" aria-hidden="true"></i>
+								</a> 
+							@endcan
 
-														<div class="column">
-															@if($row->visible == true)
-															@can('pages-disable')
-															<a class=" button is-inverted is-link is-small" href="{{route('pages.disable',$row['id'])}}">
-																<i class="fas fa-eye" aria-hidden="true"></i>
-																&nbsp; Disable
-															</a>
-															@endcan 
-															@else
-															@can('pages-enable')
-																<a class=" button is-inverted is-link is-small" href="{{route('pages.enable',$row['id'])}}">
-																	<i class="fas fa-eye" aria-hidden="true"></i>
-																	&nbsp; Enable
-																</a>
-															@endcan
-															@endif  
-														</div>
+							
+							@can('pages-destroy')
+								<form  accept-charset="UTF-8" method="post" action="{{route('pages.destroy',$row['id'])}}">
+									@csrf
+									{{ method_field('DELETE') }}{{ method_field('DELETE') }}
+									<button type="submit" class="btn btn-outline-danger">
+										<i class="fas fa-times" aria-hidden="true"></i>
+									</button>
+								</form>
+							@endcan
+						
+						</td>
+					</tr>
+					@endforeach
+				@else
+					<tr> No Data</tr>
+				@endif								
+          </tbody>
+		</table>
 
-														<div class='column'>
-															@can('pages-destroy')
-															<form  accept-charset="UTF-8" method="post" action="{{route('pages.destroy',$row['id'])}}">
-															@csrf
-															{{ method_field('DELETE') }}{{ method_field('DELETE') }}
-																<button type="submit" class="button is-rounded is-link is-small">
-																	<i class="fas fa-times" aria-hidden="true"></i>
-																	&nbsp;	Delete
-																</button>
-															</form>
-															@endcan
-														</div>
-													</div>
-												</td>
-											</tr>
-										@endforeach
-									@else
-											<tr> No Data</tr>
-									@endif								
-								</tbody>
-							</table>
-							@if ($pages->hasPages())
+		@if ($pages->hasPages())
+			<nav aria-label="Page Pagination">
+				<ul class="pagination">
+					@if ($pages->onFirstPage())
+						<li class="page-item"><a class="pagination-previous" href="#">Previous</a></li>
+						{{--  <a class="pagination-previous" disabled>Previous</a>  --}}
+					@else
+						<li class="page-item"><a class="pagination-previous"  href="{{ $pages->previousPageUrl() }}" rel="prev">Previous</a></li>
+						{{--  <a href="{{ $pages->previousPageUrl() }}" rel="prev" class="pagination-previous">Previous</a>  --}}
+					@endif
+					<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+					<li class="page-item"><a class="page-link" href="#">1</a></li>
+					<li class="page-item"><a class="page-link" href="#">2</a></li>
+					<li class="page-item"><a class="page-link" href="#">3</a></li>
+					<li class="page-item"><a class="page-link" href="#">Next</a></li>
+				</ul>
 							<nav class="pagination is-centered is-small">
 								@if ($pages->onFirstPage())
 									<a class="pagination-previous" disabled>Previous</a>
@@ -136,14 +128,11 @@
 											</li>
 								</ul>
 			
-							</nav>
-							@endif
-				 
-					  </div>
-				</div>
-			</div>
-        </section>			  
-		@include('partials.footer')
+			</nav>
+		@endif
 	</div>
-</div>
 @stop
+				 
+
+		
+	
