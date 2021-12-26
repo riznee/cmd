4b8eead168ff775117b65d22adec37fb87aff98f
Mohamed Riznee <Mohamed Riznee@mlsa.gov.mv>
2021-12-26 09:25:28 +0500

    development

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.admin')
 @section('content')
 
-	<div class ="row">
-		<div class="col">
-			<h2> Catergories </h2>
 
-		</div>
-		<div class="col">		
-			<a href="{{route('categories.create')}}" class="card-header-icon" aria-label="more options">
-				<span class="icon">
-				  <i class="fas fa-plus" aria-hidden="true"></i>
-				</span>
-			</a>
+
+	<div class ="card  border-info mb-3">		
+		<div class="card-header">
+			<div class="row">
+				<div class="col-sm-3">	 
+					<a href="{{url()->previous()}}" class="card-header-icon" style="text-decoration: none;" aria-label="more options">
+						<span class="icon">
+							<i class="fas fa-arrow-left" aria-hidden="true"></i>
+						</span>
+					</a>
+					&nbsp;&nbsp;&nbsp;&nbsp;
+					<strong>Catergories</strong>		
+				</div>
+				@can('categories-create')
+				<div class="col-sm-9">
+					<div class="float-right">
+						<a href="{{route('categories.create')}}"  aria-label="">
+							<i class="fas fa-plus" aria-hidden="true"></i>
+						</a>
+					</div>
+				</div>
+				@endcan
+			</div>
 		</div>
 	</div>
 
